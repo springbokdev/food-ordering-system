3a321b1a49414cd4258405a54710e30932c71fef
johnspangenberg <dev@springbok.space>
2024-07-12 16:38:27 +0200

    Added state changing methods to Order Entity.

---
+++ order-service/order-domain/order-domain-core/src/main/java/space/springbok/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package space.springbok.ordering.system.order.service.domain.entity;
+
+import space.springbok.ordering.system.domain.entity.AggregateRoot;
+import space.springbok.ordering.system.domain.valueobject.CustomerId;
+
+public class Customer  extends AggregateRoot<CustomerId> {
+}
