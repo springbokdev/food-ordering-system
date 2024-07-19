dcc008a0b210823257e9c4ca7fa59e3a12868a05
johnspangenberg <dev@springbok.space>
2024-07-19 17:16:02 +0200

    Implemented Kafka producer generic module.

---
+++ infrastructure/kafka/kafka-producer/src/main/java/space/springbok/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package space.springbok.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
